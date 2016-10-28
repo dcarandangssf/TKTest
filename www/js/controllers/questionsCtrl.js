0a0224785d543bef4f1296d163707b741a515c40
dcarandangssf <dcarandang@softstackfactory.org>
2016-10-28 18:42:25 +0000

    5 test passed, Continuous Integration Lesson complete

--- www/js/controllers/questionsCtrl.js
+++ www/js/controllers/questionsCtrl.js
@@ -1,45 +1,52 @@
 angular.module('starter.controllers')
-.controller('QuestionsCtrl',['$scope', '$stateParams', 'testInfo', 'TKAnswersService', '$state', '$ionicHistory',
-function($scope, $stateParams, testInfo, TKAnswersService, $state, $ionicHistory) {
-    
-$scope.Number = $stateParams.questionID;
+    .controller('QuestionsCtrl', ['$scope', '$stateParams', 'testInfo', 'TKAnswersService', '$state', '$ionicHistory',
+        function($scope, $stateParams, testInfo, TKAnswersService, $state, $ionicHistory) {
+            
+            $scope.ptorQuestionGoA = 'ptor-question-go-a' + $stateParams.questionID;
+            $scope.ptorQuestionGoB = 'ptor-question-go-b' + $stateParams.questionID;
+            $scope.ptorQuestionTextA = 'ptor-question-text-a' + $stateParams.questionID;
+            $scope.ptorQuestionTextB = 'ptor-question-text-b' + $stateParams.questionID;
+            
+            $scope.qNumber = $stateParams.questionID;
 
-testInfo.forEach(function(infoDict) {
-     if(infoDict.Answer_ID === "A")
-          $scope.questionA = infoDict;
-     if(infoDict.Answer_ID === "B")
-          $scope.questionB = infoDict;
-});
+            testInfo.forEach(function(infoDict) {
+                if (infoDict.Answer_ID === "A")
+                    $scope.questionA = infoDict;
+                if (infoDict.Answer_ID === "B")
+                    $scope.questionB = infoDict;
+            });
 
-$scope.buttonClicked = function ( option ) {
-        var category = $scope["question" + option].Style;
-        TKAnswersService.saveAnswer(category);
-       
-        if($scope.qNumber == 30) {
-           performRequest();
+            $scope.buttonClicked = function(option) {
+                var category = $scope["question" + option].Style;
+                TKAnswersService.saveAnswer(category);
+
+                if ($scope.qNumber == 30) {
+                    performRequest();
+                }
+                else {
+                    var nextqNumber = Number($scope.qNumber) + 1;
+                    $state.go('question', {
+                        questionID: nextqNumber
+                    });
+                }
+            };
+
+            $scope.goBack = function() {
+                if ($scope.qNumber > 1)
+                    TKAnswersService.eraseLastAnswer();
+                $ionicHistory.goBack();
+            };
+
+            function performRequest() {
+                var answersDict = angular.copy(TKAnswersService.getAnswers());
+                var date = new Date();
+                answersDict["createDate"] = date.toUTCString();
+                TKAnswersService.saveTest(answersDict);
+                $ionicHistory.nextViewOptions({
+                    historyRoot: true
+                });
+                $state.go('lobby');
+            };
+
         }
-        else {
-          var nextqNumber = Number($scope.qNumber) + 1;
-          $state.go('question', {questionID: nextqNumber});
-        }
-};
-
-$scope.goBack = function() {
-      if($scope.qNumber >1)
-        TKAnswersService.eraseLastAnswer();
-      $ionicHistory.goBack();
- };
- 
- function performRequest() {
-    var answersDict = angular.copy(TKAnswersService.getAnswers());
-    var date = new Date();
-    answersDict["createDate"] = date.toUTCString();
-    TKAnswersService.saveTest(answersDict);
-    $ionicHistory.nextViewOptions({
-         historyRoot: true
-    });
-    $state.go('lobby');
-}
-}]);
-
-
+    ]);
--- www/js/services/questionsService.js
+++ www/js/services/questionsService.js
@@ -1,27 +1,23 @@
 angular.module('TKTestQuestions', [])
-.service('TKTestQuestionService', ['$http', function ($http){
-    var service = this;
-    var questions = [];
-    
-    service.all = function () {
-        $http.get('files/questions.json')
-        .then(function(response){
-            if(response.status == 200)
-            {
-                questions = response.data;
-            }
-        });
-    };
-    service.getQuestion = function(questionID)
-    {
+    .service('TKTestQuestionService', ['$http', function($http) {
+        var service = this;
+        var questions = [];
+
+        service.all = function() {
+            $http.get('files/questions.json')
+                .then(function(response) {
+                    if (response.status == 200) {
+                        questions = response.data;
+                    }
+                });
+        };
+        service.getQuestion = function(questionID) {
             var results = [];
-            questions.forEach(function(question){
+            questions.forEach(function(question) {
                 //Search for questions with the specified question ID
-                if(question.Question_Number == questionID)
+                if (question.Question_Number == questionID)
                     results.push(question);
             });
             return results;
-};
-}]);
-
-
+        };
+    }]);
